18c091777d0f1f250ec1670303b7741e7829809f
ShiNxz <Amir.e@next-il.co.il>
2022-09-17 15:42:21 +0300

    .

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -9,11 +9,14 @@ import { Provider } from 'react-redux'
 // Libraries
 import { NextUIProvider } from '@nextui-org/react'
 import { ToastContainer } from 'react-toastify'
+import Script from 'next/script'
 
 const App = ({ Component, pageProps }) => {
 	return (
 		<NextUIProvider>
 			<Provider store={store}>
+				<Script src='https://cdn.enable.co.il/licenses/enable-L14488b2sr0x45vk-0922-35390/init.js%22%3E' />
+
 				<Component {...pageProps} />
 				<ToastContainer
 					rtl
