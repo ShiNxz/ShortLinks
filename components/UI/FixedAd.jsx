a35cdd8bd911e28332555e5b8480c003bc9eb62c
ShiNxz <Amir.e@next-il.co.il>
2022-09-30 08:57:57 +0300

    /

--- components/UI/FixedAd.jsx
+++ components/UI/FixedAd.jsx
@@ -1,7 +1,7 @@
 const FixedAd = () => {
 	return (
 		<div className='fixed bottom-0 left-0 right-0 p-2 text-sm bg-slate-100 text-center border-t-slate-200 border-t font-medium'>
-			צוות האתר מאחל לכם ולבני משפחתכם - חג שמח ושנה טובה ומתוקה! 🍯
+			צוות האתר מאחל לכם ולבני משפחתכם - צום קל וגמר חתימה טובה! 🌟
 		</div>
 	)
 }
